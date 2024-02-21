0b696e1c0b474b362c23784f6869805376c7397e
Nina Poliak-Karatkevich <ninapoliakkarat@gmail.com>
2024-02-21 12:42:04 -0500

    change

--- server/index.js
+++ server/index.js
@@ -11,8 +11,10 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const app = express()
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'PUT']
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 }))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
